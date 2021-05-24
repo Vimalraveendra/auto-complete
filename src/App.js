caa3c6171e571fd85a92eeecab069e3b4ef8566a
Vimal Raveendran <vimalraveendra@gmail.com>
2021-05-24 18:09:58 +0200

    created components folder & added UsersList & User components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React,{useState,useEffect} from 'react'
 import './App.css';
+import axios from 'axios'
+
+import UsersList from './components/usersList/usersList.component'
 
 function App() {
+  const [usersList,setUsersList] = useState([]);
+
+  useEffect(()=>{
+    const fetchedUsers = async()=>{
+     const response = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+     const data = await response;
+     setUsersList(data.data)
+    
+    } 
+    fetchedUsers();
+   },[])
+
   return (
     <div className="App">
-      Hello world
+     <UsersList users={usersList} />
     </div>
   );
 }
